3b53a27c5a0685fb4aa64d80b8aa133a6a4aa590
padmanabhsingh11107035-cyber <padmanabhsingh11107035@gmail.com>
2026-08-06 15:56:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ import sqlite3
 import datetime
 import logging
 import sys
+def load_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+load_css("style.css")
 
 # ==============================================================================
 # 0. LOGGING & PAGE CONFIG
