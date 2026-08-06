08a24fcb45aa8161a7402a8605f6ac193f2301c7
padmanabhsingh11107035-cyber <padmanabhsingh11107035@gmail.com>
2026-08-06 15:59:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def load_css(file_name):
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-load_css("style.css")
+load_css(".streamlit/style.css")
 
 # ==============================================================================
 # 0. LOGGING & PAGE CONFIG
